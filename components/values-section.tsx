8d793930e89db7bc1721d5baba53fe4f4f08f53f
nam.nguyen <nam.nguyen@redex.eco>
2026-01-09 07:47:27 +0700

    resolve feedback

--- components/values-section.tsx
+++ components/values-section.tsx
@@ -84,7 +84,7 @@ export default function ValuesSection() {
                 <Link href="/products">
                   <Button
                     size="lg"
-                    className="font-spaceGrotesk group h-[64px] rounded-none border-0 bg-[#DAF3F4] px-[30px] align-middle font-[Manrope] text-[18px] font-semibold leading-[150%] tracking-[0%] text-[#0036AF] shadow-lg transition-all duration-300 hover:bg-[#A0DCDD] hover:shadow-cyan-500/25"
+                    className="font-spaceGrotesk group h-[64px] rounded-none border-0 bg-[#DAF3F4] px-[30px] align-middle font-[Manrope] text-[18px] font-semibold leading-[150%] tracking-[0%] text-[#0036AF] transition-all duration-300 hover:bg-[#A0DCDD] hover:shadow-cyan-500/25"
                   >
                     View our solutions
                     <ArrowRight className="ml-[10px] h-5 w-5 transition-transform group-hover:translate-x-1" />
--- components/we-are-seeking.tsx
+++ components/we-are-seeking.tsx
@@ -8,11 +8,11 @@ import JobLocationList from './job-location-list'
 export default function WeAreSeeking() {
   return (
     <section className="relative bg-[#EDF3FF]">
-      <div className="absolute bottom-0 left-0 h-[400px] w-full bg-[url('/blog/related-blog-bg.svg')] bg-contain bg-bottom bg-no-repeat lg:h-[524px] lg:bg-cover xl:h-[832px]" />
+      <div className="absolute bottom-0 left-0 h-[400px] w-full bg-[url('/blog/related-blog-bg.svg')] bg-contain bg-bottom bg-no-repeat lg:h-[524px] lg:bg-cover xl:h-[632px] 2xl:h-[776px]" />
 
       <Suspense>
         <div className="flex justify-center">
-          <div className="container grid h-full grid-cols-12 gap-8 py-12">
+          <div className="container grid h-full grid-cols-12 gap-8 pb-36 pt-12">
             <div className="col-span-12 align-middle font-[Manrope] text-[32px] font-semibold leading-[130%] tracking-normal text-[#202222]">
               We are seeking for
             </div>
