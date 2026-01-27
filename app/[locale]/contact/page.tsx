d285fe3fdb90648bf8de2d2ea26b86a305a88727
Nguyen Trung Nam <nam.nguyen@aidi.world>
2026-01-27 14:10:03 +0700

    feat: enhance SEO

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,4 +1,40 @@
+import { getTranslations } from 'next-intl/server'
+
 import AcademyContactView from '@/components/pdt-academy/pdt-academy-contact-view'
+import { getCurrentDomain } from '@/lib/env'
+
+import type { Metadata } from 'next'
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>
+}): Promise<Metadata> {
+  const { locale } = await params
+  const t = await getTranslations({ locale, namespace: 'AcademyPage.Hero' })
+  const title = `${t('title')} ${t('highlight')}`
+  const description = t('description')
+
+  return {
+    title,
+    description,
+    twitter: {
+      title,
+      description,
+    },
+    openGraph: {
+      title,
+      description,
+      images: [
+        `${getCurrentDomain()}/product/pdt-aca-banner-hero.png`,
+        `${getCurrentDomain()}/logo-AIDI.png`,
+      ],
+      locale,
+      url: `${getCurrentDomain()}/${locale}/contact`,
+      type: 'website',
+    },
+  }
+}
 
 export default function AcademyContactPage() {
   return <AcademyContactView />
